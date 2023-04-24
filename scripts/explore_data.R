dd879fe728dc75fdbe2ac6e28ca7a8ec2a9b0c36
FSalinski <franek.salinski@gmail.com>
2023-04-24 17:39:51 +0200

    Loading data and initial exploration

---
+++ scripts/explore_data.R
@@ -0,0 +1,19 @@
+# Setting working directory to current file
+dir_path <- dirname(rstudioapi::getActiveDocumentContext()$path)
+setwd(dir_path)
+
+data_1987 <- read.csv("../data/1987.csv.bz2")
+data_2001 <- read.csv("../data/2001.csv.bz2")
+airports <- read.csv("../data/airports.csv")
+carriers <- read.csv("../data/carriers.csv")
+plane_data <- read.csv("../data/plane-data.csv")
+
+head(plane_data, 100)
+
+head(carriers)
+
+head(airports)
+
+head(data_1987)
+
+head(data_2001)
